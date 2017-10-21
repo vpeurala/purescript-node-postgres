6fd77dc902a545e539036798b7d23c2192600fc3
Ville Peurala <ville.peurala@gmail.com>
2017-10-21 17:28:32 +0300

    Add Dockerfile & docker-entrypoint.sh for customization.

--- db/start-docker-db.sh
+++ db/start-docker-db.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker container create --attach=STDIN --attach=STDOUT --attach=STDERR --hostname='purescript-node-postgres-db' --init --interactive --name='purescript-node-postgres-db' --publish=5432:5432 --tty --user=root postgres:latest /bin/bash
+docker build --tag=purescript-node-postgres-temp .
+docker container create --attach=STDIN --attach=STDOUT --attach=STDERR --hostname='purescript-node-postgres-temp' --init --interactive --name='purescript-node-postgres-temp' --publish=5432:5432 --tty --user=root postgres:latest /bin/bash
+docker start --attach --interactive purescript-node-postgres-temp
